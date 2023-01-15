ad1c4c8db0ad921c09092dedc47310ab2c87ee84
joacogambra <joaquingambra@gmail.com>
2023-01-15 00:56:53 -0300

    operadores logicos

---
+++ js/opLogicos.js
@@ -0,0 +1,15 @@
+// operadores logicos
+let and = '&&'
+let or = ' || '
+let not = ' ! '
+
+console.log(and);
+console.log(or);
+console.log(not);
+
+//and y or
+(3 > 5) && ( 1 < 10 )
+(3 > 10) || ( 1 == 1 )
+(1 > 20 ) && ( 2 > 20)
+//not
+!(1 === 5) 
