8f116dce1ec3d6943478fa56db5bd8da75dabaf3
joacogambra <joaquingambra@gmail.com>
2023-01-18 17:24:21 -0300

    metodos de array: pop,push,shift,join, includes, indexOf,etc

---
+++ js/methodsArray.js
@@ -0,0 +1,46 @@
+let frutas = ['tomate','manzana','pera','sandia','frutilla']
+let verduras = ['lechuga', 'papa', 'cebolla', 'zapallo']
+
+// El método pop() elimina el último elemento
+let fruta = frutas.pop()
+console.log(fruta); // 'frutilla'
+
+// El método push() agrega un nuevo elemento a una matriz (al final):
+verduras.push('batata') //agrega a la ultima posicion
+console.log(verduras);
+
+// El método shift() elimina el primer elemento de la matriz y "cambia" todos los demás elementos a un índice más bajo.
+let fruta2 = frutas.shift()
+console.log(fruta2); // 'tomate'
+
+// El método unshift() agrega un nuevo elemento a una matriz (al principio) y "desplaza" los elementos más antiguos
+verduras.unshift('espinaca')
+console.log(verduras);
+
+// El concat() crea una nueva matriz fusionando (concatenando) matrices existentes
+const latam = ['Argentina','Uruguay', 'Brasil']
+const north = ['EEUU', 'Mexico', 'Canada']
+// la guardo en una nueva variable sin modificar las otras
+const america = latam.concat(north)
+console.log(america);
+
+// El join() también une todos los elementos de la matriz en una cadena,
+//pero especifica el separador
+
+frutas.join(' - ')
+console.log(frutas);
+
+// splice y slice
+
+/* El includes() devuelve true si una cadena contiene una cadena especificada. De lo contrario vuelve false.
+El includes()método distingue entre mayúsculas y minúsculas. */
+
+let resultado = frutas.includes('limon') //devuelve true o false
+console.log(resultado);
+
+// El indexOf() devuelve la posición de la primera aparición de un valor en una cadena
+let resultado2 = frutas.indexOf('limon')
+console.log(resultado2); // devuelve -1 porque no esta
+let resultado3 = frutas.indexOf('sandia')
+console.log(resultado3);
+
