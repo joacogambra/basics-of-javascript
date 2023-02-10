68c33b2819f03d4be54543953e423481f1f325cd
joacogambra <joaquingambra@gmail.com>
2023-02-09 22:39:35 -0300

    add

--- intermedio/funciones.js
+++ intermedio/funciones.js
@@ -10,6 +10,28 @@ const flecha = () => {
     console.log('funcion flecha')
 }
 
+// funcion dentro de un objeto
+
+const gato = {
+    nombre: 'Kirito',
+    color: 'Naranja',
+    caminar:function (){
+        console.log(`${this.nombre} empezo a caminar`)
+    },
+}
+
+const perro = {
+    nombre: 'Lola',
+    color: 'Blanco',
+    pasear: () =>{
+        console.log(`${this.nombre} puede pasear los viernes`)
+        // aca no se veria como la funcion normal
+        // el this seria para Window (global)
+    }
+}
+
+gato.caminar()
+perro.pasear()
 
 
 
